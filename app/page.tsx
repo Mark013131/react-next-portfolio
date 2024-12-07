8f33dcab700363ef059b6df1d46293c8533f74bb
Mark013131 <167769265+Mark013131@users.noreply.github.com>
2024-12-07 10:05:34 +0900

    67pまで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
-export default function Home() {
-  return <h1>ようこそ、青山陽希のサイトへ</h1>;
+import styles from "./page.module.css";
+import Image from "next/image";
+
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>青山陽希のサイトへようこそ</h1>
+          <p className={styles.description}>
+            これは私のポートフォリオサイトです。
+            私の活動や制作物を紹介しています。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+          priority
+          sizes="100vw"
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
 }
