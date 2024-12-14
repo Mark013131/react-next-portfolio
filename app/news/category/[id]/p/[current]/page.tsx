092defdc6043317bba3596042ab17e1144056d81
Mark013131 <167769265+Mark013131@users.noreply.github.com>
2024-12-14 12:03:07 +0900

    更新

--- app/news/category/[id]/p/[current]/page.tsx
+++ app/news/category/[id]/p/[current]/page.tsx
@@ -5,13 +5,13 @@ import Pagination from "@/app/_components/Pagination";
 import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
-type PageProps = {
+type Props = {
   params: {
     id: string;
   };
 };
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: Props) {
   const category = await getCategoryDetail(params.id).catch(notFound);
 
   const { contents: news, totalCount } = await getNewsList({
